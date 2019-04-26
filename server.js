369cd8523454ac10972ff5c2c50577fc5a46e331
Michał Halicki <mhalik2@gmail.com>
2019-04-26 19:01:28 +0200

    dodano operacje crud

--- server.js
+++ server.js
@@ -29,3 +29,97 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port);
+
+
+
+
+
+
+
+
+
+
+//////// nowe metody CRUD 26.04 do integracji //////////////////////
+
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+
+// Connection URL
+const postTask = (task) => {
+    var url = 'mongodb://mhalik2:mama1011@cluster0-shard-00-00-e4xgm.gcp.mongodb.net:27017,cluster0-shard-00-01-e4xgm.gcp.mongodb.net:27017,cluster0-shard-00-02-e4xgm.gcp.mongodb.net:27017/todolist?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+        const zadanko = task;
+        console.log(zadanko);
+        //Insert a single document
+        db.collection('tasks').insertOne(zadanko, function (err, r) {
+            assert.equal(null, err);
+            assert.equal(1, r.insertedCount);
+        });
+    });
+}
+//postTask({text: 'Ogarnij się paruwo', isDone: false});
+
+const updateTask = (oldVal, newVal) => {
+    var url = 'mongodb://mhalik2:mama1011@cluster0-shard-00-00-e4xgm.gcp.mongodb.net:27017,cluster0-shard-00-01-e4xgm.gcp.mongodb.net:27017,cluster0-shard-00-02-e4xgm.gcp.mongodb.net:27017/todolist?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+        oldVal = oldVal;
+        newVal = newVal;
+        console.log(newVal);
+        //Update a single document
+        db.collection('tasks').updateOne(oldVal, {
+            $set: newVal
+        }, function (err, r) {
+            assert.equal(null, err);
+            assert.equal(1, r.matchedCount);
+            assert.equal(1, r.modifiedCount);
+        });
+    });
+}
+
+//updateTask({ "_id": ObjectId("5cc3224aaf53624cd052ef45")}, {text: 'Ogarnij się bez paruwy22'})
+
+
+const deleteTask = (task) => {
+    var url = 'mongodb://mhalik2:mama1011@cluster0-shard-00-00-e4xgm.gcp.mongodb.net:27017,cluster0-shard-00-01-e4xgm.gcp.mongodb.net:27017,cluster0-shard-00-02-e4xgm.gcp.mongodb.net:27017/todolist?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+        const zadanko = task;
+        console.log(zadanko);
+        //Insert a single document
+        db.collection('tasks').deleteOne(zadanko, function (err, r) {
+            assert.equal(null, err);
+            assert.equal(1, r.deletedCount);
+        });
+    });
+}
+
+//deleteTask({ "_id": ObjectId("5cc3224aaf53624cd052ef45") });
+
+// const getTask = (task) => {
+//     var url = 'mongodb://mhalik2:mama1011@cluster0-shard-00-00-e4xgm.gcp.mongodb.net:27017,cluster0-shard-00-01-e4xgm.gcp.mongodb.net:27017,cluster0-shard-00-02-e4xgm.gcp.mongodb.net:27017/todolist?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+//     // Use connect method to connect to the Server
+//     MongoClient.connect(url, function (err, db) {
+//         assert.equal(null, err);
+//         console.log("Connected correctly to server");
+//         const zadanko = task;
+//         console.log(zadanko);
+//         //Insert a single document
+//         var found = db.collection('tasks').findOne(zadanko, function (err, r) {
+//             assert.equal(null, err);
+//             //assert.equal(1, r.deletedCount);
+//         });
+//        console.log(found.text);
+//     });
+
+// }
+
+// getTask({ "_id": ObjectId("5cc32199f2dd370f0847a5bf") });
